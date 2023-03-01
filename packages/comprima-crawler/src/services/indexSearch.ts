6fffb3969457ed1945587aedb5f0f5fee166f288
Alexander Czigler <dev@ilix.se>
2023-03-01 17:03:55 +0100

    fix: make the code nicer

--- packages/comprima-crawler/src/services/indexSearch.ts
+++ packages/comprima-crawler/src/services/indexSearch.ts
@@ -5,13 +5,12 @@ export const indexSearch = async (level: string) => {
   return axios
   .get(`http://localhost:4000/indexSearch?query=scania&levels=${level}`, {})
   .then(({ data }) => {
-    console.log('Successfully received oauth token from FedEx.');
     return data;
   })
   .catch((e) => {
-    console.error(
-      `Failed to request oauth token from FedEx: ${e.toString()}`,
-    );
+    // console.error(
+    //   `Failed to call Comprima Adapter: ${e.toString()}`,
+    // );
     throw e;
   });
 }
