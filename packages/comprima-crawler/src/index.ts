970d6456ada8a558db0df63daacb3b9cc241eee6
Alexander Czigler <dev@ilix.se>
2023-03-01 17:03:55 +0100

    fix: make the code nicer

--- packages/comprima-crawler/src/index.ts
+++ packages/comprima-crawler/src/index.ts
@@ -1,34 +1,35 @@
 import { indexSearch } from './services/indexSearch';
 import { from, map, mergeMap, retryWhen, tap } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, retry } from 'rxjs/operators';
 
 console.log('Comprima Crawler');
+
+/*
+ * Prepare the crawler.
+ */
+const retryDelay = 1; // Seconds.
 
 const levels: number[] = [];
 for (let i = 41080; i > 41000; i--) {
   levels.push(i);
-  //const response = indexSearch(i.toString(), ['41000']).then(meow => console.log('Meow', meow));
 }
 
-console.log(levels)
+console.log('Levels', levels)
 
-const purr = from(levels).pipe(
-  // tap(level => console.log('Level', level)),
-  mergeMap(level => indexSearch(level.toString()).then(meow => {
-    console.log('Meow', meow)
-    return meow
-  }).catch(error => {
-    console.error(error)
-    return error
+/*
+ * Crawl.
+ */
+const crawlerStream = from(levels).pipe(
+  mergeMap(level => indexSearch(level.toString()).then(result => {
+    console.log(`Result (level ${level}):`, result)
+    return result
   }), 2),
   retryWhen((errors) =>
       errors.pipe(
-        tap((err) => console.error('Zip streams error, retrying in 1s...', err)),
-        delay(1000)
+        tap((err) => console.error(`Crawler streams error, retrying in ${retryDelay} s...`, err)),
+        delay(retryDelay * 1000)
       )
     ),
-  tap(meow => console.log('Meow', meow))
 )
 
-purr.subscribe()
-
+crawlerStream.subscribe()
