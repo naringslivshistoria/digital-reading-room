74c56b7bad6be051755b9a6c82d124d952a39780
Anders Norrback Bornholm <anders@bornholm.se>
2023-03-06 18:08:17 +0100

    Fixed merge conflict

--- packages/reading-room-frontend/src/App.tsx
+++ packages/reading-room-frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { Routes, Route } from "react-router-dom"
 
 import { PageSearch } from './pages/page-search'
+import { PageLogin } from './pages/login'
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <PageSearch />
+      <Routes>
+        <Route path="/" element={<PageSearch />} />
+        <Route path="/login" element={<PageLogin />} />
+      </Routes>
     </QueryClientProvider>
   )
 }
---
+++ packages/reading-room-frontend/src/pages/login.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export const PageLogin = () => {
+  return (
+    <div>
+      <div style={{ padding: '30px' }}>
+        <div>Logga in</div>
+      </div>
+    </div>
+  )
+}
