d3bade1c51c69dc11fbeabc232590ecc9b97ebd8
Alexander Czigler <dev@ilix.se>
2023-03-07 16:27:38 +0100

    chore: add default user

--- tools/token.bash
+++ tools/token.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X GET -H "Content-Type: application/json" -d '{"query":"scania","levels":41080}' http://localhost:4001/auth/generateHash?password=meow
+curl -X GET -H "Content-Type: application/json" https://search.dev.cfn.iteam.se/auth/generateHash?password=lm-rummet
