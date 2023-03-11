5cb6f81ed0cc268aa4d83a7e94d3cdd685a3eaa3
Alexander Czigler <dev@ilix.se>
2023-03-11 07:30:07 +0100

    chore: remove unused tests

--- tools/search.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-CURL -X GET -H "Content-Type: application/json" -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzZTg0ZDRiNS03NmQwLTQ4MTctYjI5NS1lNjY4M2QwMWYwNDEiLCJ1c2VybmFtZSI6ImRldi10ZWFtIiwiaWF0IjoxNjc4Mjc3NzExLCJleHAiOjE2NzgyODg1MTF9.euMec3YwyO3b6eNeGGFK8TvNwNzQqDjKU5utxluRvMw" https://search.dev.cfn.iteam.se/search?query=*aaa*
