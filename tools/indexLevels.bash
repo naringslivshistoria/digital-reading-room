d1e76ee1d2a6f08237d7d931329f4c84b8ae9ff8
Alexander Czigler <dev@ilix.se>
2023-03-11 07:23:11 +0100

    fix: clarify

--- tools/indexLevels.bash
+++ tools/indexLevels.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-for i in {41080..41000}
+for i in {41010..41000} # 41080 is the highest level.
 do
   echo "Getting $i"
-  /usr/bin/curl "http://localhost:4000/indexSearch?query=scania&levels=$i"
+
+  # Local
+  # /usr/bin/curl "http://localhost:4000/indexSearch?query=scania&levels=$i"
+
+  # CI
+  # /usr/bin/curl "https://comprima.dev.cfn.iteam.se/indexSearch?query=scania&levels=$i"
   echo "\n"
 done
