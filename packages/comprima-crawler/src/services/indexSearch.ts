d2da1cf586434aba75b48eb20c8f869ddc984327
Alexander Czigler <dev@ilix.se>
2023-03-13 11:53:45 +0100

    docs: note the use of the query parameter

--- packages/comprima-crawler/src/services/indexSearch.ts
+++ packages/comprima-crawler/src/services/indexSearch.ts
@@ -3,7 +3,8 @@ import config from '../common/config'
 import log from '../common/log'
 
 export const indexSearch = async (level: string) => {
-  const url = `${config.comprimaUrl}/indexSearch?query=scania&levels=${level}`
+  const query = 'ignored' // NOTE: This value is not used.
+  const url = `${config.comprimaUrl}/indexSearch?query=${query}&levels=${level}`
   log.debug(`Calling comprima adapter on ${url}`)
   return axios
   .get(url, {})
