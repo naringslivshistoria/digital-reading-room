85bf1037f45b9dfb3a5e2ad1aa129a5eecfb33a3
Alexander Czigler <dev@ilix.se>
2023-03-20 13:47:17 +0100

    chore: hard-coding levels for now

--- packages/comprima-crawler/src/services/postgresAdapter/index.ts
+++ packages/comprima-crawler/src/services/postgresAdapter/index.ts
@@ -1,10 +1,12 @@
 import log from '../../common/log'
 
-export const getUnindexedLevels = async (count: number) => {
-  log.debug(`Request ${count} unindexed levels from postgres`)
+export const getUnindexedLevels = async (): Promise<{lower: number, upper: number}> => {
   // TODO: Get unindexed levels from postgres
   // TODO: Mark levels as in progress in postgres
 
   // TODO: Remove this mock
-  return Promise.resolve([41000,41001,41003])
+  return Promise.resolve({
+    lower: 41000,
+    upper: 42000
+  })
 }
