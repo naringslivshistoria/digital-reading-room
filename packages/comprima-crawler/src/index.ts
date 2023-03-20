68ad865233bbabb232448ba528ae05dee0236266
Alexander Czigler <dev@ilix.se>
2023-03-20 13:51:17 +0100

    chore: moving crawl logic to a service

--- packages/comprima-crawler/src/index.ts
+++ packages/comprima-crawler/src/index.ts
@@ -4,50 +4,35 @@ import { getUpdatedLevels, indexSearch } from './services/comprimaService/index'
 import { getUnindexedLevels } from './services/postgresAdapter/index';
 import config from './common/config';
 import log from './common/log';
+import { getSubRange } from './helpers/arrayHelper';
+import { crawlLevels } from './services/crawlerService';
 
 log.info('Comprima Crawler', config);
 
 // TODO: Check mode - either crawl unindexed levels or reindex updated levels (or ids?).
 const mode = config.mode;
-let levels
+// let levels
+let levelPromise
 switch (config.mode) {
   case 'index':
-    levels = getUnindexedLevels(1) // TODO: config.crawler.batchSize
+    levelPromise = getUnindexedLevels // TODO: config.crawler.batchSize
     break;
-  case 'update':
-    levels = getUpdatedLevels()
-    break;
+  // case 'update':
+  //   levelPromise = getUpdatedLevels
+  //   break;
   default:
-    log.error(`Unknown mode ${mode}!`)
     log.warn(`CRAWLER_MODE must be either 'index' or 'update'`)
-    process.exit(1)
+    log.error(`Unknown mode ${mode}!`)
+    throw new Error(`Unknown mode ${mode}!`)
 }
 
 /*
  * Setup levels.
  */
-levels.then(levels => {
-  log.info('Processing levels', levels)
+levelPromise().then(levels => {
+  return crawlLevels(levels)
+})
+.then(result => log.info(`Crawl complete: ${result}`))
+.catch(error => log.error('Crawl failed!', error))
 
-  /*
-   * Crawl.
-   */
-  const crawlerStream = from(levels).pipe(
-    // TODO: Send levels as an array to indexSearch
-    mergeMap(level => indexSearch(levels).then(result => {
-      log.info(`✅ Level ${level}`, result)
-      return result
-    }).catch(error => {
-      log.error(`Crawler was unable to process level ${level}!`)
-      return error
-    }), config.crawler.concurrency),
-    retry({count: config.crawler.retryCount, delay: config.crawler.retryDelay * 1000})
-    // TODO: What happens when retry counts are exhausted?
-  )
-
-  /*
-   * Start the crawler.
-   */
-  crawlerStream.subscribe()
-});
 
---
+++ packages/comprima-crawler/src/services/crawlerService/index.ts
@@ -0,0 +1,36 @@
+import config from "../../common/config"
+import log from "../../common/log"
+import { getSubRange } from "../../helpers/arrayHelper"
+import { indexSearch } from "../comprimaService"
+
+export interface Range {
+  lower: number
+  upper: number
+}
+
+export const crawlLevels = async (range: Range): Promise<boolean> => {
+  // TODO: Mark range as in progress in postgres
+
+  const cursor = {
+    lower: range.lower,
+    upper: range.upper + config.crawler.batchSize
+  }
+  // TODO: Crawl 10 at a time
+  do {
+    cursor.upper += config.crawler.batchSize
+    const levels = getSubRange(cursor.lower, range.upper, config.crawler.batchSize)
+
+    log.info(`Crawling levels`, cursor)
+    log.debug('Levels', levels)
+    try {
+      const result = await indexSearch(levels)
+      log.info(`✅ Levels ${cursor.lower}–${cursor.upper}`, result)
+    } catch (error) {
+      log.error(`Crawler was unable to process levels ${cursor.lower}–${cursor.upper}!`)
+    }
+
+    cursor.lower += config.crawler.batchSize
+  } while (cursor.lower <= range.upper)
+
+  return true
+}
