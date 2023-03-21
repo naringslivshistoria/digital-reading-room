5f59db1306dfb195b8a8870070ee65819e9217b2
Alexander Czigler <dev@ilix.se>
2023-03-21 14:45:28 +0100

    refactor: logging

--- packages/comprima-crawler/src/common/log.ts
+++ packages/comprima-crawler/src/common/log.ts
@@ -4,65 +4,54 @@ import config from './config';
 export interface Log {
   debug: (message: string, data?: object) => void;
   error: (title: string, error?: Error) => void;
-  info: (message: string, data?: object | string, ...rest: string[]) => void;
-  warn: (title: string, error?: Error) => void;
+  info: (message: string, data?: object | string) => void;
+  warn: (title: string, data?: object) => void;
 }
 
-const logLevelIsAtLeastDebug = config.logLevel.toUpperCase() === 'DEBUG';
-const logLevelIsAtLeastInfo =
-  config.logLevel.toUpperCase() === 'INFO' || logLevelIsAtLeastDebug;
-const logLevelIsAtLeastWarn =
-  config.logLevel.toUpperCase() === 'WARN' || logLevelIsAtLeastInfo;
+const LEVEL = config.logLevel.toUpperCase();
 
-  export default {
-    debug: (message: string, data?: object) => {
-      if (!logLevelIsAtLeastDebug) {
-        return;
-      }
-  
-      if (data) {
-        console.debug(
-          `${chalk.whiteBright.bold('DEBUG')} ${chalk.gray(
-            message
-          )}`, data
-        );
-      } else {
-        console.debug(
-          `${chalk.whiteBright.bold('DEBUG')} ${chalk.gray(
-            message
-          )}`
-        );
-      }
-    },
-    error: (title: string, error?: Error) => {
-      console.error(`${chalk.redBright.bold('ERROR')} ${chalk.red(title)}`);
-      if (error) {
-        console.error(error);
-      }
-    },
-    info: (message: string, data?: object | string, ...rest: string[]) => {
-      if (!logLevelIsAtLeastInfo) {
-        return;
-      }
+const levelIsAtLeastDebug = LEVEL === 'DEBUG';
+const levelIsAtLeastInfo = LEVEL === 'INFO' || levelIsAtLeastDebug;
+const levelIsAtLeastWarn = LEVEL === 'WARN' || levelIsAtLeastInfo;
 
-      console.log(
-        `${chalk.whiteBright.bold('INFO ')} ${chalk.white(
-          message
-        )}`,
-        data,
-        ...rest
-      );
-    },
-    warn: (title: string, error?: Error) => {
-      if (!logLevelIsAtLeastWarn) {
-        return;
-      }
-  
-      console.log(
-        `${chalk.red.bold('WARN ')} ${chalk.white(title)}`
-      );
-      if (error) {
-        console.error(error);
-      }
-    },
-  } as Log
+const print = (logFn: Function, titleFn: Function, messageFn: Function, title: string, message: string, data?: object | string) => {
+  if (data) {
+    logFn(
+      titleFn(title),
+      messageFn(message),
+      data
+    );
+  } else {
+    logFn(
+      titleFn(title),
+      messageFn(message)
+    );
+  }
+}
+
+export default {
+  debug: (message: string, data?: object) => {
+    if (!levelIsAtLeastDebug) {
+      return;
+    }
+
+    print(console.debug, chalk.whiteBright.bold, chalk.gray, 'DEBUG', message, data)
+  },
+  error: (title: string, error?: Error) => {
+    print (console.error, chalk.redBright.bold, chalk.red, 'ERROR', title, error)
+  },
+  info: (message: string, data?: object | string) => {
+    if (!levelIsAtLeastInfo) {
+      return;
+    }
+
+    print (console.log, chalk.whiteBright.bold, chalk.white, 'INFO ', message, data)
+  },
+  warn: (title: string, data?: object) => {
+    if (!levelIsAtLeastWarn) {
+      return;
+    }
+
+    print (console.log, chalk.red.bold, chalk.white, 'WARN ', title, data)
+  },
+} as Log
