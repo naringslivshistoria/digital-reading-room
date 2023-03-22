26ce6a7fd8df9355aea1a238f900704efabc9a1a
Alexander Czigler <dev@ilix.se>
2023-03-22 17:14:31 +0100

    fix: linting

--- packages/comprima-crawler/src/common/log.ts
+++ packages/comprima-crawler/src/common/log.ts
@@ -14,20 +14,23 @@ const levelIsAtLeastDebug = LEVEL === 'DEBUG';
 const levelIsAtLeastInfo = LEVEL === 'INFO' || levelIsAtLeastDebug;
 const levelIsAtLeastWarn = LEVEL === 'WARN' || levelIsAtLeastInfo;
 
-const print = (logFn: Function, titleFn: Function, messageFn: Function, title: string, message: string, data?: object | string) => {
+const print = (
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  logFn: Function,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  titleFn: Function,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  messageFn: Function,
+  title: string,
+  message: string,
+  data?: object | string
+) => {
   if (data) {
-    logFn(
-      titleFn(title),
-      messageFn(message),
-      data
-    );
+    logFn(titleFn(title), messageFn(message), data);
   } else {
-    logFn(
-      titleFn(title),
-      messageFn(message)
-    );
+    logFn(titleFn(title), messageFn(message));
   }
-}
+};
 
 export default {
   debug: (message: string, data?: object) => {
@@ -35,23 +38,44 @@ export default {
       return;
     }
 
-    print(console.debug, chalk.whiteBright.bold, chalk.gray, 'DEBUG', message, data)
+    print(
+      console.debug,
+      chalk.whiteBright.bold,
+      chalk.gray,
+      'DEBUG',
+      message,
+      data
+    );
   },
   error: (title: string, error?: Error) => {
-    print (console.error, chalk.redBright.bold, chalk.red, 'ERROR', title, error)
+    print(
+      console.error,
+      chalk.redBright.bold,
+      chalk.red,
+      'ERROR',
+      title,
+      error
+    );
   },
   info: (message: string, data?: object | string) => {
     if (!levelIsAtLeastInfo) {
       return;
     }
 
-    print (console.log, chalk.whiteBright.bold, chalk.white, 'INFO ', message, data)
+    print(
+      console.log,
+      chalk.whiteBright.bold,
+      chalk.white,
+      'INFO ',
+      message,
+      data
+    );
   },
   warn: (title: string, data?: object) => {
     if (!levelIsAtLeastWarn) {
       return;
     }
 
-    print (console.log, chalk.red.bold, chalk.white, 'WARN ', title, data)
+    print(console.log, chalk.red.bold, chalk.white, 'WARN ', title, data);
   },
-} as Log
+} as Log;
