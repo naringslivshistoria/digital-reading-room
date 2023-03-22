3a71f3d3e472d39f03bf00d6a38653b38edfb57b
Alexander Czigler <dev@ilix.se>
2023-03-22 14:53:28 +0100

    fix: levels seed

--- packages/comprima-crawler/seeds/dev/levels.js
+++ packages/comprima-crawler/seeds/dev/levels.js
@@ -9,18 +9,28 @@ exports.seed = async function (knex) {
   const path = './seeds/dev/levels.csv';
 
   return new Promise((resolve, reject) => {
+    let rows = [];
     return fs
       .createReadStream(path)
       .pipe(parse({ delimiter: ';', from_line: 2 }))
       .on('data', async ([depositor, archivist, level]) => {
-        await knex('levels').insert({
-          archivist,
-          depositor,
-          level,
-        });
+        console.log('Row', depositor, archivist, level);
+
+        rows.push({ depositor, archivist, level });
       })
       .on('end', function () {
-        return resolve('ok');
+        console.log('CSV file read. Inserting into database...');
+
+        let inserts = [];
+        rows.forEach((row) => {
+          process.stdout.write('.');
+          inserts.push(knex('levels').insert(row));
+        });
+
+        return Promise.all(inserts).then(() => {
+          console.log('done!');
+          return resolve();
+        });
       })
       .on('error', function (error) {
         console.log(error.message);
