c38aff83ac39609b7e4afe62dc0e24ecf0da4960
Anders Norrback Bornholm <anders@bornholm.se>
2023-04-11 13:44:34 +0200

    Fixed security for attachments

--- packages/reading-room-search/src/api.ts
+++ packages/reading-room-search/src/api.ts
@@ -1,10 +1,12 @@
-import KoaRouter from '@koa/router';
+import KoaRouter from '@koa/router'
 import { routes as searchRoutes } from './services/searchService'
 import { routes as thumbnailRoutes } from './services/thumbnailService'
+import { routes as documentRoutes } from './services/documentService'
 
-const router = new KoaRouter();
+const router = new KoaRouter()
 
 searchRoutes(router)
 thumbnailRoutes(router)
+documentRoutes(router)
 
 export default router
