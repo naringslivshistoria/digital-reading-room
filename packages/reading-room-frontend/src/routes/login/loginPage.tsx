9b8110154de95ec7ed912956133c6b388c965d7d
Anders Norrback Bornholm <anders@bornholm.se>
2023-05-02 08:44:48 +0200

    Logs in user automatically

--- packages/reading-room-frontend/src/routes/login/loginPage.tsx
+++ packages/reading-room-frontend/src/routes/login/loginPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TextField, Button, Alert, Grid, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
 
@@ -8,8 +8,8 @@ import { SiteHeader } from '../../components/siteHeader'
 export const PageLogin = () => {
   const { onLogin } = useAuth()
 
-  const [username, setUsername] = useState<string>()
-  const [password, setPassword] = useState<string>()
+  const [username, setUsername] = useState<string>('dev-team')
+  const [password, setPassword] = useState<string>('lm-rummet')
   const [error, setError] = useState<boolean>(false)
 
   const doLogin = async () => {
@@ -20,6 +20,10 @@ export const PageLogin = () => {
     }
   }
 
+  useEffect(() => {
+    doLogin()
+  })
+
   return (
     <>
     <SiteHeader />
@@ -28,8 +32,8 @@ export const PageLogin = () => {
       <Grid item xs={10} md={5} lg={3} justifyContent='flex-start'>
         <Typography variant='h2' sx={{ marginTop: '75px', marginBottom: '20px' }}>Logga in</Typography>
         <Stack rowGap={4} justifyContent='flex-start' >
-          <TextField id="username" onChange={(e) => { setUsername(e.target.value) }} label="Användarnamn" variant="standard" />
-          <TextField id="password" onChange={(e) => { setPassword(e.target.value) }} label="Lösenord" variant="standard" type="password" />
+          <TextField id="username" onChange={(e) => { setUsername(e.target.value) }} value={username} label="Användarnamn" variant="standard" />
+          <TextField id="password" onChange={(e) => { setPassword(e.target.value) }} value={password} label="Lösenord" variant="standard" type="password" />
           <Button variant="text" onClick={doLogin} sx={{ marginTop: 2, borderRadius: 0, bgcolor: '#53565a', color: 'white', '&:hover': { backgroundColor: 'secondary.main', color: 'white'} }}>Logga in</Button>
           { error &&
             <Alert severity="error">Inloggning misslyckades!</Alert>
