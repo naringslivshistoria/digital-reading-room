67f6c89f73d82db968d57ab094afa3978ee780eb
Anders Norrback Bornholm <anders@bornholm.se>
2023-05-22 15:13:28 +0200

    Changed loop in comprima adapter to save memory

--- packages/comprima-adapter/src/api.ts
+++ packages/comprima-adapter/src/api.ts
@@ -1,79 +1,90 @@
-import KoaRouter from '@koa/router';
-import comprima from './services/comprimaService';
-import index from './services/indexingService';
-import { routes as comprimaRoutes } from './services/comprimaService';
+import KoaRouter from '@koa/router'
+import comprima from './services/comprimaService'
+import index from './services/indexingService'
+import { routes as comprimaRoutes } from './services/comprimaService'
 
-const router = new KoaRouter();
+const router = new KoaRouter()
 
 router.get('/', async (ctx) => {
-  ctx.body = 'Comprima Adapter API';
-});
+  ctx.body = 'Comprima Adapter API'
+})
 
 router.get('/healthz', async (ctx) => {
   ctx.body = {
     comprima: 'Not OK',
     elasticSearch: 'Not OK',
-  };
-
-  try {
-    await comprima.healthCheck();
-    ctx.body.comprima = 'OK';
-  } catch (error) {
-    console.error(error);
-    ctx.status = 500;
   }
 
   try {
-    await index.healthCheck();
-    ctx.body.elasticSearch = 'OK';
+    await comprima.healthCheck()
+    ctx.body.comprima = 'OK'
   } catch (error) {
-    console.error(error);
-    ctx.status = 500;
+    console.error(error)
+    ctx.status = 500
   }
-});
+
+  try {
+    await index.healthCheck()
+    ctx.body.elasticSearch = 'OK'
+  } catch (error) {
+    console.error(error)
+    ctx.status = 500
+  }
+})
 
 router.get('/index/:documentId', async (ctx) => {
   if (!ctx.params.documentId) {
-    ctx.status = 400;
-    ctx.body = { errorMessage: 'Missing document id' };
-    return;
+    ctx.status = 400
+    ctx.body = { errorMessage: 'Missing document id' }
+    return
   }
 
   try {
-    const document = await comprima.getDocument(
-      parseInt(ctx.params.documentId)
-    );
-    await index.indexDocument(document);
-    ctx.body = { result: 'success' };
+    const document = await comprima.getDocument(parseInt(ctx.params.documentId))
+    await index.indexDocument(document)
+    ctx.body = { result: 'success' }
   } catch (err) {
-    ctx.status = 500;
-    ctx.body = { results: 'error: ' + err };
+    ctx.status = 500
+    ctx.body = { results: 'error: ' + err }
   }
-});
+})
 
 router.get('/indexlevel', async (ctx) => {
   if (!ctx.query.level) {
-    ctx.status = 400;
-    ctx.body = { errorMessage: 'Missing parameter: level' };
-    return;
+    ctx.status = 400
+    ctx.body = { errorMessage: 'Missing parameter: level' }
+    return
   }
 
-  let skip = 0;
+  let skip = 0
   if (ctx.query.skip) {
     skip = parseInt(
       Array.isArray(ctx.query.skip) ? ctx.query.skip[0] : ctx.query.skip
-    );
+    )
   }
 
   try {
     const level = Array.isArray(ctx.query.level)
       ? ctx.query.level[0]
-      : ctx.query.level;
-    const results = await comprima.search(level, skip);
+      : ctx.query.level
+    const results = await comprima.search(level, skip)
 
-    let successful = 0;
+    let successful = 0
 
-    await Promise.all(
+    // Do not Promise.all this as concurrent indexing of many documents
+    // will use a lot of memory.
+    for (const document of results) {
+      try {
+        document.level = level
+        await index.indexDocument(document)
+        console.log('Successfully indexed document', document.id)
+        successful++
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    /*await Promise.all(
       results.map(async (document) => {
         try {
           document.level = level;
@@ -85,17 +96,17 @@ router.get('/indexlevel', async (ctx) => {
 
         return document;
       }, 0)
-    );
+    );*/
 
     ctx.body = {
       result: { successful: successful, failed: results.length - successful },
-    };
+    }
   } catch (err) {
-    ctx.status = 500;
-    ctx.body = { results: 'error: ' + err };
+    ctx.status = 500
+    ctx.body = { results: 'error: ' + err }
   }
-});
+})
 
-comprimaRoutes(router);
+comprimaRoutes(router)
 
-export default router;
+export default router
--- tools/token.bash
+++ tools/token.bash
@@ -2,9 +2,9 @@
 
  PASSWORD=$1
 
- RESULT=$(curl -s -X GET -H "Content-Type: application/json" https://search.dev.cfn.iteam.se/auth/generateHash?password=$1)
+ RESULT=$(curl -s -X GET -H "Content-Type: application/json" https://localhost:4001/auth/generateHash?password=$1)
 
  echo "Hash: $(echo $RESULT | jq -r '.password')"
  echo "Salt: $(echo $RESULT | jq -r '.salt')"
  
-curl -X GET -H "Content-Type: application/json" -d '{"query":"scania","levels":41080}' http://localhost:4001/auth/generateHash?password=meow
+#curl -X GET -H "Content-Type: application/json" -d '{"query":"scania","levels":41080}' http://localhost:4001/auth/generateHash?password=meow
