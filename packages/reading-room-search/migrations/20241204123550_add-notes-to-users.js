1d1acf315cf47919ec369130cd1b0938ad25cc4d
Simon Gustafsson <simon.gustafsson@iteam.se>
2024-12-04 13:39:55 +0100

    Adds a field for adding notes on users

---
+++ packages/reading-room-search/migrations/20241204123550_add-notes-to-users.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table('users', (table) => {
+    table.text('notes')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table('users', (table) => {
+    table.dropColumn('notes')
+  })
+}
