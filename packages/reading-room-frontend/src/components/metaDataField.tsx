09acc682a687e5c9a5efd52fb26104629015f527
Simon <simon.gustafsson@iteam.se>
2025-01-31 08:03:53 +0100

    feat: Add volume field linking to search with dynamic filter (#133)

--- packages/reading-room-frontend/src/components/metaDataField.tsx
+++ packages/reading-room-frontend/src/components/metaDataField.tsx
@@ -1,4 +1,5 @@
 import { Typography } from '@mui/material'
+import { Link, useSearchParams } from 'react-router-dom'
 
 import { Document } from '../common/types'
 
@@ -11,16 +12,29 @@ export const MetaDataField = ({
   fieldName?: string
   heading: string
 }) => {
+  const [searchParams] = useSearchParams()
+
   const getFieldValueString = (fieldName: string) => {
-    if (
-      !document.fields[fieldName] ||
-      !document.fields[fieldName].value ||
-      document.fields[fieldName].value === ''
-    ) {
+    if (!document.fields[fieldName]?.value) {
       return '-'
-    } else {
-      return document.fields[fieldName].value
     }
+
+    if (fieldName === 'volume') {
+      const existingFilter = searchParams.get('filter') || ''
+      const newFilter = existingFilter
+        .split('||')
+        .filter((f) => !f.startsWith('volume::'))
+        .concat(`volume::${document.fields.volume.value}`)
+        .join('||')
+
+      const searchString = `/search?query=${
+        newFilter ? `&filter=${encodeURIComponent(newFilter)}` : ''
+      }`
+
+      return <Link to={searchString}>{document.fields[fieldName].value}</Link>
+    }
+
+    return document.fields[fieldName].value
   }
 
   return (
